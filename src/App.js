67384025c2871d4b01173e26a6eda3970f5701e7
nik-coder <nikpennan@gmail.com>
2022-04-03 14:47:00 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import React, { Component } from "react";
+import React from "react";
 
 import TodoList from "./components/TodoList";
 
-class App extends Component {
+const App = () => {
   constructor(props) {
     super(props);
 
